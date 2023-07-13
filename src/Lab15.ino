5f2039f439be8855234812410d16b5c7c62872a4
ShoubhitP <spusulur24@gmail.com>
2023-07-13 14:55:17 -0500

    hi

---
+++ src/Lab15.ino
@@ -0,0 +1,48 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+#include "oled-wing-adafruit.h"
+
+OledWingAdafruit display;
+
+void setup()
+{
+  display.setup();
+  pinMode(D5, INPUT_PULLUP);
+  Serial.begin(9600);
+  while (!Serial.isConnected())
+  {
+  }
+
+  Watchdog.init(WatchdogConfiguration().timeout(5s));
+  Watchdog.start();
+
+  if (System.resetReason() == RESET_REASON_WATCHDOG)
+  {
+    refreshDisplay();
+    display.println("Watchdog reset");
+    display.display();
+  }
+  else
+  {
+    refreshDisplay();
+    display.println("Normal reset");
+    display.display();
+    }
+}
+
+void loop()
+{
+  display.loop();
+  if (!digitalRead(D5)) // D5 is high
+  {
+    Watchdog.refresh();
+  }
+}
+void refreshDisplay()
+{
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+}
